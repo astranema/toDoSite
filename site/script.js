e1870290d293e81d249c2ea3da2f255e31fb79e3
luna 3 <93050744+astranema@users.noreply.github.com>
2025-12-24 17:05:06 -0500

    Added task delete and edit, refactored
    Used the existing header code to allow tasks to be editing and deleted, creating a minimum viable product
    refactored element creation code, specifically the addList function

--- site/script.js
+++ site/script.js
@@ -17,38 +17,64 @@ function addList() {
 	// making the list
 	const list = document.createElement("div");
 	list.classList.add("list");
-	// making the header
-	const listHeader = document.createElement("div");
-	listHeader.classList.add("list-header");
-	// making the titleContainer
-	const listHeaderTitleContainer = document.createElement("div");
-	listHeaderTitleContainer.classList.add("list-header-title-container");
-	// making the title
-	const listHeaderTitle = document.createElement("div");
-	listHeaderTitle.textContent = "Title";
-	listHeaderTitle.addEventListener("click", function () {
-		makeInputBox(listHeaderTitleContainer, listHeaderTitle);
-	});
-	// making the trash button
-	const deleteList = document.createElement("div");
-	deleteList.classList.add("delete-list");
-	deleteList.addEventListener("click", function () {
-		list.remove();
-	});
+	// making the list header
+	const listHeader = makeListHeader(list);
 	// making the + button
+	const addTaskButton = makeAddTaskButton(list);
+	// adding everything
+	listContainer.appendChild(list);
+	list.appendChild(listHeader);
+	list.appendChild(addTaskButton);
+}
+
+function makeAddTaskButton(list) {
 	const addTaskButton = document.createElement("div");
 	addTaskButton.textContent = "+";
 	addTaskButton.classList.add("add-task-button");
 	addTaskButton.addEventListener("click", function () {
 		addTask(list);
 	})
-	// adding everything
-	listContainer.appendChild(list);
-	list.appendChild(listHeader);
-	list.appendChild(addTaskButton);
-	listHeader.appendChild(deleteList);
-	listHeader.appendChild(listHeaderTitleContainer);
-	listHeaderTitleContainer.appendChild(listHeaderTitle);
+	return addTaskButton;
+}
+
+function makeListHeader(list) {
+	// creating the listHeader
+	const listHeader = document.createElement("div");
+	listHeader.classList.add("list-header");
+	// making the container for the title and the title
+	const titleContainer = makeListTitleContainer();
+	// making the trash button
+	const deleteButton = makeDeleteButton(list);
+	// appending children to the listHeader
+	listHeader.appendChild(deleteButton);
+	listHeader.appendChild(titleContainer);
+	// returning the ending value of listHeader
+	return listHeader;
+}
+
+function makeDeleteButton(element) {
+	const deleteButton = document.createElement("div");
+	deleteButton.classList.add("delete-button");
+	deleteButton.addEventListener("click", function () {
+		element.remove();
+	});
+	return deleteButton;
+}
+
+function makeListTitleContainer() {
+	// making the titleContainer
+	const listTitleContainer = document.createElement("div");
+	listTitleContainer.classList.add("list-header-title-container");
+	// making the title
+	const listHeaderTitle = document.createElement("div");
+	listHeaderTitle.textContent = "New List";
+	listHeaderTitle.addEventListener("click", function () {
+		makeInputBox(listTitleContainer, listHeaderTitle);
+	});
+	// making the listHeaderTitle a child of the container
+	listTitleContainer.appendChild(listHeaderTitle);
+	// returning title container
+	return listTitleContainer;
 }
 
 function makeInputBox(titleContainer, text) {
@@ -57,19 +83,37 @@ function makeInputBox(titleContainer, text) {
 	inputBox.classList.add("list-title-input-box");
 	inputBox.addEventListener('keydown', function (event) {
 		if (event.key === "Enter") {
-			text.style.display = "block";
-			text.textContent = event.target.value;
-			inputBox.remove();
+			confirmTextInput(text, inputBox);
 		}
+	});
+	inputBox.addEventListener("blur", function () {
+		confirmTextInput(text, inputBox);
 	});
 	titleContainer.appendChild(inputBox);
 	inputBox.focus();
 }
 
+// assumes textBox's display should be block naturally, and is
+// currently none
+function confirmTextInput(textBox, inputBox) {
+	if (inputBox.value != "") {
+		textBox.textContent = inputBox.value;
+	}
+	textBox.style.display = "block";
+	inputBox.remove();
+}
+
 function addTask(list) {
-	const newTask = document.createElement("div");
-	newTask.textContent = "pee";
-	list.appendChild(newTask);
+	const task = document.createElement("div");
+	task.classList.add("task");
+	// making text container
+	const textContainer = makeListTitleContainer();
+	task.appendChild(textContainer);
+	// making delete button
+	const deleteButton = makeDeleteButton(task);
+	task.appendChild(deleteButton);
+	// appending task to list
+	list.appendChild(task);
 }
 
 function updateDarkMode() {
